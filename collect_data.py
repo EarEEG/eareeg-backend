59a855eeb28c401e86cd0f5192469a86e6f5150a
Tyler Stuessi <tyler.stuessi@gmail.com>
2017-02-28 12:06:24 -0500

    Separate out data collection class

--- collect_data.py
+++ collect_data.py
@@ -1,31 +1,10 @@
 import NeuroPy.NeuroPy as NP
+import mindwave_data
 import time
-
-# class dedicated to keeping track of data
-class mindwave_data(object):
-    def __init__(self):
-        self.attention_list = []
-        self.meditation_list = []
-        self.rawValue_list = []
-        self.delta_list = []
-        self.theta_list = []
-        self.lowAlpha_list = []
-        self.highAlpha_list = []
-        self.lowBeta_list = []
-        self.highBeta_list = []
-        self.lowGamma_list = []
-        self.midGamma_list = []
-        self.poorSignal_list = []
-        self.blinkStrength_list = []
-    def print_list(self):
-        for i, j in vars(self).iteritems():
-            print " ".join([str(x) for x in j])
-
-
 
 # global variable because data collection is done through
 # callback functions
-global_mindwave_data = mindwave_data()
+global_mindwave_data = mindwave_data.mindwave_data()
 
 # annoyingly long list of callback functions for each 
 # individual variable
---
+++ mindwave_data.py
@@ -0,0 +1,21 @@
+# mindwave_data.py
+# class dedicated to keeping track of data
+class mindwave_data(object):
+    def __init__(self):
+        self.attention_list = []
+        self.meditation_list = []
+        self.rawValue_list = []
+        self.delta_list = []
+        self.theta_list = []
+        self.lowAlpha_list = []
+        self.highAlpha_list = []
+        self.lowBeta_list = []
+        self.highBeta_list = []
+        self.lowGamma_list = []
+        self.midGamma_list = []
+        self.poorSignal_list = []
+        self.blinkStrength_list = []
+
+    def print_list(self):
+        for i, j in vars(self).iteritems():
+            print " ".join([str(x) for x in j])
